994b9a50719cb647818778898208ec60cbdc85e0
Remy Dale <jem.dale@gmail.com>
2022-02-26 06:50:46 -0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//create a function that adds two numbers and alerts the sum
+function add(a, b) {
+    return (a + b);
+}
+
+console.log(add(1, 3));
+
+//create a function that multiplys three numbers and console logs the product
+function product(a, b, c) {
+    return (a * b * c);
+}
+
+console.log(product(1, 2, 3));
+
+//create a function that divides two numbers and returns the ???
+function remainder(a, b) {
+    return (a % b);
+}
+
+console.log(remainder(10, 3));
